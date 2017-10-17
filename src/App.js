f5597ea4b89afebb5d53783bdda001011a72d97f
alexmorten <alexandrius.martinus@gmx.de>
2017-10-17 13:55:00 +0200

    Better

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './styles/App.css';
 import socketIOClient from "socket.io-client";
 import SlackMessage from './components/SlackMessage/SlackMessage';
+import List from './components/List/List';
 const endpoint = 'http://localhost:4001';
 class App extends Component {
   state={
@@ -17,12 +18,14 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to a temp. Example</h1>
         </header>
         <p className="App-intro">
-        {this.state.messages.map((message)=>{
-          return ( <div className="distance-top"><SlackMessage message={message}/></div>)
-        })}
+          <List>
+            {this.state.messages.map((message)=>{
+              return ( <div className="distance-top"><SlackMessage message={message}/></div>)
+            })}
+          </List>
         </p>
       </div>
     );
